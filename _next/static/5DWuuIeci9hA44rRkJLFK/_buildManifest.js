cb95b9c8665c700ef0b4ffb9065b4386c37439bb
Umberto <antrax148@hotmail.com>
2023-04-16 23:29:30 +0200

    first website version

---
+++ _next/static/5DWuuIeci9hA44rRkJLFK/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/8bff10f8b944ac04.css","static/chunks/pages/index-a43105f580a9b038.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
